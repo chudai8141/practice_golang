6b76a83ba4518ff2e091d2b9b07d0d7ba36771e0
chudai8141 <chu.dai.kimukimu.0420@gmail.com>
2021-12-29 18:28:49 +0900

    finished calculation oeprator for go

---
+++ chapter4/calc_operator.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 2)
+
+	fmt.Println(5 - 2)
+
+	fmt.Println(5 * 4)
+
+	fmt.Println(60 / 3)
+
+	fmt.Println(9 % 4)
+
+	fmt.Println("ABC" + "DE")
+
+	n := 0
+	n += 4
+	fmt.Println(n)
+
+	n++
+	fmt.Println(n)
+
+	n--
+	fmt.Println(n)
+
+	s := "ABC"
+	s += "DEF"
+	fmt.Println(s)
+
+}
