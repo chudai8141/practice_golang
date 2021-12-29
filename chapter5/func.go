88d21defe0846aa351ed9411107fea77aa97ed6c
chudai8141 <chu.dai.kimukimu.0420@gmail.com>
2021-12-29 19:29:42 +0900

    finished chapter5

---
+++ chapter5/func.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func Plus(x int, y int) int {
+	return x + y
+}
+
+func Div(x, y int) (int, int) {
+	q := x / y
+	r := x / y
+
+	return q, r
+}
+
+func Double(price int) (result int) {
+	result = price * 2
+	return
+
+}
+
+func Noreturn() {
+	fmt.Println("No Return")
+	return
+}
+
+func main() {
+	i := Plus(1, 2)
+	fmt.Println(i)
+
+	// i2, i3 := Div(9, 3)
+	i2, _ := Div(9, 3)
+	fmt.Println(i2)
+
+	i4 := Double(1000)
+	fmt.Println(i4)
+
+	Noreturn()
+
+}
