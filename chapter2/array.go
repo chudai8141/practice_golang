8991be7dee46fffe834e9a503d80c6068329ebbd
chudai8141 <chu.dai.kimukimu.0420@gmail.com>
2021-12-29 17:41:16 +0900

    array.go finished

--- chapter2/array.go
+++ chapter2/array.go
@@ -7,4 +7,29 @@ func main() {
 	fmt.Println(arr1)
 
 	fmt.Printf("%T\n", arr1)
+	var arr2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arr2)
+
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
+
+	// [...] -> 要素の数を自動的に数える
+	arr4 := [...]string{"C", "D"}
+	fmt.Println(arr4)
+	fmt.Printf("%T\n", arr4)
+
+	fmt.Println(arr1[0])
+	fmt.Println(arr2[0])
+	fmt.Println(arr2[1])
+	fmt.Println(arr2[2-1])
+
+	arr2[2] = "C"
+	fmt.Println(arr2)
+
+	// var arr5 [4]int
+	// arr5 = arr1
+	// fmt.Println(arr5)
+
+	fmt.Println(len(arr1))
+
 }
