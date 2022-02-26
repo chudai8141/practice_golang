1e0f6d03b86b4f67d89121c77056e20c0a6712dc
chudai8141 <chu.dai.kimukimu.0420@gmail.com>
2022-02-26 11:35:39 +0900

    finished chapter9

---
+++ chapter9/interface_stringer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	A int
+	B string
+}
+
+func (p *Point) String() string {
+	return fmt.Sprintln("<<%v, %v>>", p.A, p.B)
+}
+
+func main() {
+	p := &Point{100, "ABC"}
+	fmt.Println(p)
+
+}
